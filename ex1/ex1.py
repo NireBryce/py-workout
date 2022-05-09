50456dd0fc8acaf60517b85f6576009639ed3dc7
Elly Ard <eocowden@gmail.com>
2022-05-08 22:48:20 -0400

    still learning git UI in VScode

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,4 +1,17 @@
+import random
+def guessing_game():
+    # random intiger between 1 and 100 (inclusive)
+    random_number = random.randint(1, 100)
+    
+    # take user input and have them guess the number
+    guess = input("Guess a number between 1 and 100: ")
+    while guess != 
+    pass
+
 def main():
+    
+    guessing_game()
+    
     pass
 
 
